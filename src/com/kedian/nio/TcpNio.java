6e52a8ad01902380b8e51fcfabf31ea3d0ed673b
git_wzh <git_wzh@kedian.com>
2019-07-22 15:36:19 +0800

    UDP使用Nio

--- src/com/kedian/nio/TcpNio.java
+++ src/com/kedian/nio/TcpNio.java
@@ -17,47 +17,51 @@ import java.util.Scanner;
  * @author wzh
  * @description
  * @create 2019-07-21 21:54
- *
- *  * 一、使用 NIO 完成网络通信的三个核心：
- *  *
- *  * 1. 通道（Channel）：负责连接
- *  *
- *  * 	   java.nio.channels.Channel 接口：
- *  * 			|--SelectableChannel
- *  * 				|--SocketChannel
- *  * 				|--ServerSocketChannel
- *  * 				|--DatagramChannel
- *  *
- *  * 				|--Pipe.SinkChannel
- *  * 				|--Pipe.SourceChannel
- *  *
- *  * 2. 缓冲区（Buffer）：负责数据的存取
- *  *
- *  * 3. 选择器（Selector）：是 SelectableChannel 的多路复用器。用于监控 SelectableChannel 的 IO 状况
- *  *
+ * <p>
+ * * 一、使用 NIO 完成网络通信的三个核心：
+ * *
+ * * 1. 通道（Channel）：负责连接
+ * *
+ * * 	   java.nio.channels.Channel 接口：
+ * * 			|--SelectableChannel
+ * * 				|--SocketChannel
+ * * 				|--ServerSocketChannel
+ * * 				|--DatagramChannel
+ * *
+ * * 				|--Pipe.SinkChannel
+ * * 				|--Pipe.SourceChannel
+ * *
+ * * 2. 缓冲区（Buffer）：负责数据的存取
+ * *
+ * * 3. 选择器（Selector）：是 SelectableChannel 的多路复用器。用于监控 SelectableChannel 的 IO 状况
+ * *
  */
-public class NonBlocingkNIO {
+public class TcpNio {
 
     @Test
-    public void client()   {
+    public void client() {
         try {
             //1.获取通道
-            SocketChannel sChannel=SocketChannel.open(new InetSocketAddress("127.0.0.1",9898));
+            SocketChannel sChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 9898));
             //2.切换到非阻塞模式
             sChannel.configureBlocking(false);
             //3.分配指定大小缓冲区
-            ByteBuffer buf=ByteBuffer.allocate(1024);
+            ByteBuffer buf = ByteBuffer.allocate(1024);
             //4.发送数据给客户端
-            Scanner scanner=new Scanner(System.in);
+            /*Scanner scanner = new Scanner(System.in);
             System.out.println("请输入：");
-            while (scanner.hasNext()){
-                String str=scanner.next();
+            while (scanner.hasNext()) {
+                String str = scanner.next();
                 System.out.println(str);
-                buf.put((LocalDateTime.now().toString()+"\n"+str).getBytes());
+                buf.put((LocalDateTime.now().toString() + "\n" + str).getBytes());
                 buf.flip();
                 sChannel.write(buf);
                 buf.clear();
-            }
+            }*/
+            buf.put((LocalDateTime.now().toString()).getBytes());
+            buf.flip();
+            sChannel.write(buf);
+            buf.clear();
             //5.关闭通道
             sChannel.close();
         } catch (IOException e) {
@@ -67,10 +71,10 @@ public class NonBlocingkNIO {
 
 
     @Test
-    public void server()   {
+    public void server() {
         try {
             //1.获取通道
-            ServerSocketChannel ssChannel=ServerSocketChannel.open();
+            ServerSocketChannel ssChannel = ServerSocketChannel.open();
             //2.切换到非阻塞模式
             ssChannel.configureBlocking(false);
             //3.绑定连接
@@ -80,29 +84,29 @@ public class NonBlocingkNIO {
             //5.将通道注册到选择器中
             ssChannel.register(selector, SelectionKey.OP_ACCEPT);
             //6.轮询获取选择器上已经“准备就绪”的事件
-            while (selector.select()>0){
+            while (selector.select() > 0) {
                 //7.获取当前选择器中所有注册的“选择键是已就绪的监听事件”
                 Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-                while (iterator.hasNext()){
+                while (iterator.hasNext()) {
                     //8.获取事件
                     SelectionKey selectionKey = iterator.next();
                     //9.判断是否是准备就绪事件
-                    if (selectionKey.isAcceptable()){
+                    if (selectionKey.isAcceptable()) {
                         //10.若是“准备就绪”，获取客户端连接通道
                         SocketChannel sChannel = ssChannel.accept();
                         //11.切换到非阻塞模式
                         sChannel.configureBlocking(false);
                         //12.将该通道注册到选择器
-                        sChannel.register(selector,SelectionKey.OP_READ);
-                    }else if (selectionKey.isReadable()){
+                        sChannel.register(selector, SelectionKey.OP_READ);
+                    } else if (selectionKey.isReadable()) {
                         //13.获取当前选择器是“读就绪”状态通道
-                        SocketChannel sChannel= (SocketChannel) selectionKey.channel();
+                        SocketChannel sChannel = (SocketChannel) selectionKey.channel();
                         //14.读取数据
-                        ByteBuffer buf=ByteBuffer.allocate(1024);
-                        int len=0;
-                        while ((len=sChannel.read(buf))>0){
+                        ByteBuffer buf = ByteBuffer.allocate(1024);
+                        int len = 0;
+                        while ((len = sChannel.read(buf)) > 0) {
                             buf.flip();
-                            System.out.println(new String(buf.array(),0,len));
+                            System.out.println(new String(buf.array(), 0, len));
                             buf.clear();
                         }
                     }
---
+++ src/com/kedian/nio/UdpNio.java
@@ -0,0 +1,59 @@
+package com.kedian.nio;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ * @author wuzh
+ * @version V1.0
+ * @Package com.kedian.nio
+ * @Description:
+ * @date 2019/7/22
+ */
+public class UdpNio {
+
+    @Test
+    public void send() throws IOException {
+        DatagramChannel dc = DatagramChannel.open();
+        dc.configureBlocking(false);
+        ByteBuffer buf = ByteBuffer.allocate(1024);
+        String str = new Date().toString();
+        buf.put(str.getBytes());
+        buf.flip();
+        dc.send(buf, new InetSocketAddress("127.0.0.1", 9898));
+        buf.clear();
+        dc.close();
+    }
+
+    @Test
+    public void receive() throws IOException {
+        DatagramChannel dc = DatagramChannel.open();
+        dc.configureBlocking(false);
+        dc.bind(new InetSocketAddress(9898));
+        Selector selector = Selector.open();
+        dc.register(selector, SelectionKey.OP_READ);
+        while (selector.select() > 0) {
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            while (iterator.hasNext()) {
+                SelectionKey sk = iterator.next();
+                if (sk.isReadable()) {
+                    ByteBuffer buf = ByteBuffer.allocate(1024);
+                    dc.receive(buf);
+                    buf.flip();
+                    System.out.println(new String(buf.array(), 0, buf.limit()));
+                    buf.clear();
+                }
+            }
+            iterator.remove();
+        }
+    }
+}
